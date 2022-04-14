1fd3a3ff1bfcc2dfc5061f8c3a0564f48d318efc
egorov-maksim <maxim.egorov@me.com>
2022-04-14 20:15:08 +0300

    Homework1
    Practice in linux terminal
    working with files and shell scripts

---
+++ homework.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+#script for homework
+
+cd homework_1
+mkdir folder1_1 folder2_1 folder3_1
+cd folder1_1
+touch file1_1.txt file2_1.txt file3_1.txt file4_1.json file5_1.json
+mkdir folder1_1_1 folder2_1_1 folder3_1_1
+ls -la
+mv file1_1.txt file2_1.txt folder2_1_1
+
