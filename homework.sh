ece5ac0caf7b26636b385ca93d32b6836c7b4f8b
egorov-maksim <101495447+egorov-maksim@users.noreply.github.com>
2022-04-19 17:40:42 +0300

    Script_HW_1.sh
    Updating info and comments

--- homework.sh
+++ homework.sh
@@ -1,6 +1,5 @@
-#!bin/bash
 #script for homework
-
+#!bin/bash
 cd homework_1
 mkdir folder1_1 folder2_1 folder3_1
 cd folder1_1
@@ -8,4 +7,3 @@ touch file1_1.txt file2_1.txt file3_1.txt file4_1.json file5_1.json
 mkdir folder1_1_1 folder2_1_1 folder3_1_1
 ls -la
 mv file1_1.txt file2_1.txt folder2_1_1
-
